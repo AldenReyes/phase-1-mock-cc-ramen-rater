c721ca5a27c7a9210bad76f3c9fb2021197785fc
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-04-29 04:42:59 -0700

    Adds listener to ramen images and on click will populate card with info from db.json

--- src/index.js
+++ src/index.js
@@ -15,16 +15,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const loadRamenImages = function () {
   fetch("http://localhost:3000/ramens")
-  .then(res => res.json())
-  .then(arr => appendImageToMenu(arr))
+    .then((res) => res.json())
+    .then((arr) => appendImageToMenu(arr))
+    .then((arr) => addListeners())
+    .catch(console.log("Failed to load"));
 };
 
 const appendImageToMenu = function (ramen) {
-  ramen.map(obj =>{
-    const img = document.createElement("img")
-    img.setAttribute("src", `${obj.image}`)
-    img.setAttribute("alt", `${obj.name} from ${obj.restaurant}`)
-    img.setAttribute("id", `ramen-${obj.id}`)
-    document.getElementById("ramen-menu").appendChild(img)
-  })
-}
+  ramen.map((obj) => {
+    const img = document.createElement("img");
+    img.setAttribute("src", `${obj.image}`);
+    img.setAttribute("alt", `${obj.name} from ${obj.restaurant}`);
+    img.setAttribute("id", `${obj.id}`);
+    img.setAttribute("class", "ramen-img");
+    document.getElementById("ramen-menu").appendChild(img);
+  });
+};
+
+const addListeners = function () {
+  const imgs = Array.from(
+    document.getElementsByClassName("ramen-img"),
+    addListener
+  );
+  return imgs;
+};
+
+const addListener = function (arr) {
+  arr.addEventListener("click", populateCard);
+};
+
+const populateCard = function (e) {
+  const ramenSrc = document.querySelector(".detail-image");
+  const ramenName = document.querySelector(".name");
+  const restaurantName = document.querySelector(".restaurant");
+  const rating = document.getElementById("rating-display");
+  const comment = document.getElementById("comment-display");
+  ramenSrc.setAttribute("src", `${e.target.src}`);
+  fetch(`http://localhost:3000/ramens/${e.target.id}`)
+    .then((res) => res.json())
+    .then((e) => {
+      ramenName.textContent = e.name;
+      restaurantName.textContent = e.restaurant;
+      rating.textContent = e.rating;
+      comment.textContent = e.comment;
+    });
+};
